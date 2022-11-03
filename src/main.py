ec976728000077b4c4fc23c5f084245035d3696c
Edgar Granados Perez <edgar.granados.perez@gmail.com>
2022-11-03 05:52:33 -0500

    add

--- src/main.py
+++ src/main.py
@@ -1,17 +1,6 @@
-from datetime import date
-import holidays
+def main(): 
+    print("hello world")
 
-def main():
-    # Select country
-    uk_holidays = holidays.UnitedKingdom()
-    # Print all the holidays in UnitedKingdom in year 2018
-    # Print all the holidays in UnitedKingdom in year 2018
-for ptr in holidays.UnitedStates(years = 2022).items():
-	print(ptr)
-
-for ptr in holidays.UnitedStates(years = 2023).items():
-	print(ptr)
-
-
-if __name__ == "main":
+if __name__ == "__main__":
     main()
+
--- src/webCrawler.py
+++ src/webCrawler.py
@@ -1,14 +1,21 @@
-from html.parser import HTMLParser
-from bs4 import BeautifulSoup
-import requests
+from selenium import webdriver
 
-def  webcrawler():
-    URL = "https://cbu.edu/"
-    page = requests.get(URL)
-    soup = BeautifulSoup(page.content, "html.parser")
-
-    print(soup)
+# the chrome version for this application is chrome 107
+class webcrawler:
+    macPath = ''
+    windowsPath = 'C:\\Users\\myPC\\iCloudDrive\Desktop\\Git Projects\\senior_project\\src\\chromedriver.exe'
+    url= 'https://pubmed.ncbi.nlm.nih.gov/'
+    browser = webdriver.Chrome(executable_path= windowsPath)
 
 
-if __name__ == "webcrawler":
-    webcrawler()
+
+    def setbrowserOS():
+         while True:
+            os = input("which operating system are you running this code on? mac or windows: ")
+            if os in ("mac","windows"):
+                break
+            else :
+                print("please type the correct os either \"mac\" or \"os\"")
+        
+         print("chose the correct value")
+        
