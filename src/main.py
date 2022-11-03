4d15ecb285efd7c0ba647f45ecd81fc49bc0205c
Edgar Granados <edgar.granados.perez@gmail.com>
2022-11-03 18:49:45 -0500

    add

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
+import webcrawler
+
 def main(): 
+    print('starting webcrawler to open')
+    webcrawler()
     print("hello world")
 
 if __name__ == "__main__":
--- src/webCrawler.py
+++ src/webCrawler.py
@@ -1,21 +1,27 @@
 from selenium import webdriver
 
 # the chrome version for this application is chrome 107
-class webcrawler:
-    macPath = ''
-    windowsPath = 'C:\\Users\\myPC\\iCloudDrive\Desktop\\Git Projects\\senior_project\\src\\chromedriver.exe'
-    url= 'https://pubmed.ncbi.nlm.nih.gov/'
-    browser = webdriver.Chrome(executable_path= windowsPath)
-
-
-
-    def setbrowserOS():
-         while True:
+def setbrowserOS():
+        macPath = '/Users/edgargranadosperez/Desktop/Git Projects/senior_project/src/chromedrivermac'
+        windowsPath = 'C:\\Users\\myPC\\iCloudDrive\Desktop\\Git Projects\\senior_project\\src\\chromedriver.exe'
+        browser = None
+        while True:
             os = input("which operating system are you running this code on? mac or windows: ")
             if os in ("mac","windows"):
-                break
+                if os == "mac":
+                    browser = webdriver.Chrome(executable_path= macPath)
+                    print("chose mac")
+                    return browser
+                elif os == "windows":
+                    browser = webdriver.Chrome(executable_path=windowsPath)
+                    print("chose windows")
+                    return browser
             else :
                 print("please type the correct os either \"mac\" or \"os\"")
-        
-         print("chose the correct value")
-        
+
+def startwebcrawler():
+    pubmed ='https://pubmed.ncbi.nlm.nih.gov/'
+    browser = setbrowserOS()
+    browser.get(pubmed)
+
+startwebcrawler()
